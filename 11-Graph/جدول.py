f0aaa2a60ec08bb65894bbcac58797d164981475
Ali Mahdi <seyyedalimehdi@gmail.com>
2025-05-31 23:12:20 +0330

    solve some more

--- 11-Graph/جدول.py
+++ 11-Graph/جدول.py
@@ -1,25 +1,59 @@
+import sys
+sys.setrecursionlimit(10**7)
+
 n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-m = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False]*n for _ in range(n)]
 
-print(m)
-edges = [[] for _ in range(n)]
 
-for i in range(n - 1):
+dirs = [
+    (-1, -1), (-1, 0), (-1, 1),
+    (0, -1),          (0, 1),
+    (1, -1),  (1, 0), (1, 1)
+]
+
+def in_bounds(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def dfs(x, y, height):
+    stack = [(x, y)]
+    cells = []
+    visited[x][y] = True
+    while stack:
+        cx, cy = stack.pop()
+        cells.append((cx, cy))
+        for dx, dy in dirs:
+            nx, ny = cx + dx, cy + dy
+            if in_bounds(nx, ny) and not visited[nx][ny] and grid[nx][ny] == height:
+                visited[nx][ny] = True
+                stack.append((nx, ny))
+    return cells
+
+peaks = 0
+valleys = 0
+
+for i in range(n):
     for j in range(n):
-        if i == j:
-            continue
-        if i == 0 and j != n - 1:
-            print("hey")
-            if m[i][j] == m[i][j + 1]:
-                edges[i][j].append(j)
-                edges[j].append(i)
+        if not visited[i][j]:
+            cells = dfs(i, j, grid[i][j])
+            h = grid[i][j]
+            is_peak = True
+            is_valley = True
 
-            pass
-        elif i == n - 1:
-            pass
-        if j == 0:
-            pass
-        elif j == n - 1:
-            pass
-print(edges)
+            for (cx, cy) in cells:
+                for dx, dy in dirs:
+                    nx, ny = cx + dx, cy + dy
+                    if in_bounds(nx, ny):
+                        nh = grid[nx][ny]
+                        if nh != h:
+                            if nh > h:
+                                is_peak = False
+                            if nh < h:
+                                is_valley = False
+            if is_peak:
+                peaks += 1
+            if is_valley:
+                valleys += 1
+
+print(peaks, valleys)
